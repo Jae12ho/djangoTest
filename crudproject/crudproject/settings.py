93a8624d40cea877336c6f3e40a890bce54531fb
Jaeho Yang <ho4121e@gmail.com>
2022-07-28 01:07:05 +0900

    collectstatic

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -120,11 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import os
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
